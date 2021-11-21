a63a834827565fd424d669642faeb947a4bf3eec
chellesjord <m.kayejordan@gmail.com>
2021-11-20 18:59:20 -0600

    passwork prompts information saved as array

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment code here
+
+//random number generator to pick from array of password settings.
+var randomNumber = function () {
+  var value = Math.floor(Math.random() * 4);
+  return value;
+}
+
+var lowercaseset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseset = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericset = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialset = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "_", "~", "+"];
+
 //password length question
-//need to set parameters to numeric values
 var length = function () {
   var passwordlength = window.prompt("Choose your password character LENGTH by typing a number between 8 and 128.");
 
@@ -18,6 +29,7 @@ var length = function () {
     //console.log and alert for password length
     console.log("Password length: " + passwordlength);
     window.alert("You have chosen a password LENGTH of " + passwordlength + " characters long.");
+    return passwordlength
   }
 }
 
@@ -57,7 +69,7 @@ var numeric = function () {
   if (passwordnumeric) {
     console.log("numeric: " + passwordnumeric);
     window.alert("You have chosen to include NUMERIC characters in your password.");
-    return numeric;
+    return passwordnumeric;
   }
   else {
     console.log("numeric: " + passwordnumeric);
@@ -72,7 +84,7 @@ var special = function () {
   if (passwordspecial) {
     console.log("special: " + passwordspecial);
     window.alert("You have chosen to include SPECIAL characters in your password.");
-    return special;
+    return passwordspecial;
   }
   else {
     console.log("special: " + passwordspecial);
@@ -81,16 +93,34 @@ var special = function () {
   }
 }
 
-//when 'generate password' button is clicked, 
-//function for prompts for password criteria start
-function generatePassword() {
-  length();
-  lower();
-  upper();
-  numeric();
-  special();
-};
+var questions = function () {
+  var passcharacters = [lower(), upper(), numeric(), special()];
+  //not working but why
+  if (lower === false && upper === false && numeric === false && special === false) {
+    window.alert("You have not chosen any characters for your password. Please try again.");
+    generatePassword();
+  }
+  else {
+    window.alert("Thank you for your selections. Your password is being generated...");
+  }
 
+  return passcharacters;
+}
+
+function generatePassword() {
+  //function for prompts for password criteria start
+  var passwordlength = length();
+  var charctercheck = questions();
+  console.log(charctercheck);
+  for (var i = 0; i < passwordlength; i++) {
+    console.log("apple");
+  }
+  //console.log(passwordlength);
+  //figure out a way to loop for that password length
+  //console.log(charctercheck);
+  //make a randomizer, to pick a number in the array,
+  // if the number in array is false, pick another number;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -103,6 +133,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
